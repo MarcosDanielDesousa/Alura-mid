3654087f099c45816374fd9408d77af180eb6679
MarcosDanielDesousa <106745438+MarcosDanielDesousa@users.noreply.github.com>
2023-10-27 08:55:40 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-document.querySelector('.tecla_pom');
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play('');
+    }
+    
+    const ListaDeTeclas = document.querySelectorALL('.tecla')
+    
+    let contador = 0;
+
+while(contador < 9){
+   ListaDeTeclas[contador].onclick = function(){
+
+   };
+
+   contador = contador + 1;
+}
+
+
+
+
+
